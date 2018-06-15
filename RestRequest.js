0d690fea4bbff86b6b886ed6edcd9c3a7c504b0e
Volha Valasevich <volha_valasevich@epam.com>
2018-06-15 11:17:11 +0300

    add task 4

---
+++ RestRequest.js
@@ -0,0 +1,43 @@
+const request = require("request");
+let url = "http://services.groupkt.com/country/get/";
+let flag = false;
+if (process.argv.length >= 3) {
+    const countrycode = process.argv[2];
+    switch (countrycode.length) {
+        case 2: {
+            url = url + "iso2code/" + countrycode.toUpperCase();
+            break;
+        }
+        case 3: {
+            url = url + "iso3code/" + countrycode.toUpperCase();
+            break;
+        }
+        default: {
+            url = url.replace("get/", "search?text=") + countrycode;
+            flag = true;
+        }
+    }
+    request(url, { json: true }, (err, res, body) => {
+        if (err) { 
+            return console.log(err); 
+        }
+        let countryjson = body.RestResponse.result;
+        if (typeof(countryjson) != "undefined") {
+            if (!flag) {
+                console.log("Country name: " + countryjson.name);
+                console.log("ISO 2 code: " + countryjson.alpha2_code);
+                console.log("ISO 3 code: " + countryjson.alpha3_code); 
+            } else {
+                for (let i = 0; i < countryjson.length; i++) {
+                    console.log("Country name: " + countryjson[i].name);
+                    console.log("ISO 2 code: " + countryjson[i].alpha2_code);
+                    console.log("ISO 3 code: " + countryjson[i].alpha3_code);
+                }
+            }   
+        } else {
+            console.log("Country not found");
+        }
+    });
+} else {
+    console.log("Enter country name or code");
+}
