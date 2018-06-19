24039ad062bdd0b58fa8086bebfe56e1a2b6e420
Volha Valasevich <volha_valasevich@epam.com>
2018-06-19 14:32:59 +0300

    add foreach instead of for

--- JsonToXlsx.js
+++ JsonToXlsx.js
@@ -1,56 +1,51 @@
 const XLSX = require("xlsx");
 const fs = require("fs");
-const jsonregexp = /.+.json$/;
-let inputdir = "./";
-let outputdir = "./ConvertedFiles";
-let list_of_files;
-let jsonobj;
+const minimist = require("minimist";)
 
-const args = require("minimist")(process.argv.slice(2));
+function main() {
+    const jsonregexp = /.+.json$/;
+    let inputdir = "./";
+    let outputdir = "./ConvertedFiles";
+    let list_of_files;
+    let jsonobj;
+    const args = minimist(process.argv.slice(2));
 
-if (args.inputDir != null) {
-    inputdir = args.inputDir;
-} 
-
-if (args.outputDir != null) {
-    outputdir = args.outputDir;
-    if (checkOutputDir(outputdir) === 0) {
-        console.log("Directory [" + outputdir + "] does not exist and cannot be created.");
-        return;
-    }        
-} 
-
-try {
-    list_of_files = fs.readdirSync(inputdir);
-} catch (err) {
-    console.log("Cannot read files from [" + inputdir + "]. Check if it exists or is avaliable.");
-    return;
-}
-
-
-for (let i=0; i<list_of_files.length; i++) {
-
-    if (list_of_files[i].toLowerCase().match(jsonregexp)) {
-
-        let jsonstring = fs.readFileSync(inputdir + list_of_files[i])
-
-        try {
-            jsonobj = JSON.parse(jsonstring);
-        } catch (err) {
-            console.log("File [" + list_of_files[i] + "] contains invalid data and was ignored");
-            continue;
-        }
-
-        let workbook = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(workbook, jsontosheet(jsonobj), list_of_files[i]);
-
-        try {
-            XLSX.writeFile(workbook, outputdir + "/" + list_of_files[i].replace("json", "xlsx")); 
-        } catch (err) {
-            console.log("Cannot write " + list_of_files[i] + " in " + outputdir);
+    if (args.inputDir != null) inputdir = args.inputDir;
+    if (args.outputDir != null) {
+        outputdir = args.outputDir;
+        if (checkOutputDir(outputdir) === 0) {
+            console.log("Directory [" + outputdir + "] does not exist and cannot be created.");
             return;
         }
     }
+
+    try {
+        list_of_files = fs.readdirSync(inputdir);
+    } catch (err) {
+        console.log("Cannot read files from [" + inputdir + "]. Check if it exists or is avaliable.");
+        return;
+    }
+
+
+    list_of_files.forEach(element => {
+        if (list_of_files[i].toLowerCase().match(jsonregexp)) {
+            const jsonstring = fs.readFileSync(inputdir + list_of_files[i])
+            try {
+                jsonobj = JSON.parse(jsonstring);
+            } catch (err) {
+                console.log("File [" + list_of_files[i] + "] contains invalid data and was ignored");
+                continue;
+            }
+            const workbook = XLSX.utils.book_new();
+            XLSX.utils.book_append_sheet(workbook, jsontosheet(jsonobj), list_of_files[i]);
+            try {
+                XLSX.writeFile(workbook, outputdir + "/" + list_of_files[i].replace("json", "xlsx"));
+            } catch (err) {
+                console.log("Cannot write " + list_of_files[i] + " in " + outputdir);
+                return;
+            }
+        }
+    })
 }
 
 function jsontosheet(jsonobj) {
@@ -74,3 +69,5 @@ function checkOutputDir(outputdir) {
         }
     }
 }
+
+main();
